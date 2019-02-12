364d372cf1367016dfa8cbf92acc873b523ba936
Amir Geffen <amirgeffen@gmail.com>
2019-02-12 17:34:48 +0100

    adding people temlplet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firstLec';
-  bobi = 'grass';
+  title = 'title ao ronni';
+  content = 'xyz';
 }
